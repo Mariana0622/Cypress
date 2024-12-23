756f0131912a9b66bec756671394762859892b46
Mariana0622 <maranakicuk@gmail.com>
2024-12-23 17:31:51 +0300

    Update login_maryana.cy.js

--- new_cypress-main/main/cypress/e2e/login_maryana.cy.js
+++ new_cypress-main/main/cypress/e2e/login_maryana.cy.js
@@ -45,11 +45,11 @@ describe('Проверка авторизации', function () {
     })
     it('проверка на приведение к строчным буквам в логине', function () {
         cy.visit('https://login.qa.studio');
-        cy.get('#mail').type('german@dolnikov.ru');
+        cy.get('#mail').type('GerMan@Dolnikov.ru');
         cy.get('#pass').type('iLoveqastudio1');
         cy.get('#loginButton').click();
         cy.get('#messageHeader').should('be.visible');
-        cy.get('#messageHeader').contains('Такого логина или пароля нет');
+        cy.get('#messageHeader').contains('Авторизация прошла успешно');
         cy.get('#exitMessageButton > .exitIcon').should('be.visible');
     })
 })
